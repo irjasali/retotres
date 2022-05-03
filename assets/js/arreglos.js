017ab5c01f48f6b7a331ad2b5948c93b132065b9
Irving J. S. Linares <irjasali@gmail.com>
2022-05-03 11:37:38 -0500

    First commit

---
+++ assets/js/arreglos.js
@@ -0,0 +1,32 @@
+class fibonacciNumber {
+  constructor(numero) {
+    this.numero = numero;
+  }
+
+  calcularNumero(numeroF) {
+   
+    numeroF = parseInt(numeroF);        
+    const arregloFinal = [];
+    if (numeroF === 0) {
+      arregloFinal.push(0);
+    } else if (numeroF === 0 || numeroF >= 2) {
+      arregloFinal.push(0);
+      arregloFinal.push(1);
+    }
+    for (let x = 2; x <= numeroF - 1; x++) {
+      arregloFinal.push(arregloFinal[x - 1] + arregloFinal[x - 2]);
+    }
+    document.getElementById("resultado").innerHTML +=" "+arregloFinal;
+    return arregloFinal;
+  }
+}
+
+const fibonacci =()=>{
+var valor = document.getElementById('num').value;
+const numberOne = new fibonacciNumber();
+document.getElementById("resultado").innerHTML +=" "+numberOne.calcularNumero(valor);
+}
+const limpiar=()=>{
+  document.getElementById("num").value="XXX";
+	document.getElementById("resultado").innerHTML ="La serie Fibonacci es:";
+}
